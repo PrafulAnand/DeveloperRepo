7e73746f9fcd8f47b04e0ed0c10fb9fbb2b22e2f
PrafulAnand <prafulanand724@gmail.com>
2021-06-22 04:50:53 +0530

    updated css for foooter and button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return(
         <div className = "footer">
-            <p>Create with ❤ by Praful Anand</p>
+            <p>Created with ❤ by Praful Anand</p>
         </div>
 
     );
